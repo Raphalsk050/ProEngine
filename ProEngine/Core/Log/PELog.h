3c1c7904af28aae227ad0825c96597687018d502
Rafael Aragao Morais <ram.morais50@gmail.com>
2025-06-13 19:14:45 -0300

    Add missing folders
    Signed-off-by: Rafael Aragao Morais <ram.morais50@gmail.com>

---
+++ ProEngine/Core/Log/PELog.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <memory>
+#include <spdlog/spdlog.h>
+
+namespace ProEngine {
+  class PELog {
+  public:
+    // Initialize the Logger (call once at startup)
+    static void Init(const std::string &engineName = "PENGINE");
+
+    // Retrieve the core logger
+    inline static std::shared_ptr<spdlog::logger> &GetCoreLogger() { return s_CoreLogger; }
+
+    // (Optional) Retrieve a client/user logger
+    inline static std::shared_ptr<spdlog::logger> &GetClientLogger() { return s_ClientLogger; }
+
+  private:
+    static std::shared_ptr<spdlog::logger> s_CoreLogger;
+    static std::shared_ptr<spdlog::logger> s_ClientLogger;
+  };
+}
+//#define DEBUG
+#ifdef DEBUG
+
+#define PENGINE_CORE_TRACE(...)    ::ProEngine::PELog::GetCoreLogger()->trace(__VA_ARGS__)
+#define PENGINE_CORE_INFO(...)     ::ProEngine::PELog::GetCoreLogger()->info(__VA_ARGS__)
+#define PENGINE_CORE_WARN(...)     ::ProEngine::PELog::GetCoreLogger()->warn(__VA_ARGS__)
+#define PENGINE_CORE_ERROR(...)    ::ProEngine::PELog::GetCoreLogger()->error(__VA_ARGS__)
+#define PENGINE_CORE_CRITICAL(...) ::ProEngine::PELog::GetCoreLogger()->critical(__VA_ARGS__)
+
+#else
+
+#define PENGINE_CORE_TRACE(...)    do {} while(false)
+#define PENGINE_CORE_INFO(...)     do {} while(false)
+#define PENGINE_CORE_WARN(...)     do {} while(false)
+#define PENGINE_CORE_ERROR(...)    do {} while(false)
+#define PENGINE_CORE_CRITICAL(...) do {} while(false)
+
+#endif
